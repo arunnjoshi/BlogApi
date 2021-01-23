c1c2035b2c6cb26b56817da415469e6b347b7b13
Arun Joshi <43997581+arunjoshi1@users.noreply.github.com>
2021-01-23 23:05:06 +0530

    Update Blog.cs

--- BlogApi/Models/Blog.cs
+++ BlogApi/Models/Blog.cs
@@ -1,16 +1,15 @@
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
-using System;
 
 namespace BlogApi.Models
 {
-	public class Blog
-	{
+    public class Blog
+    {
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
-        public string Id   { get; set; }
+        public string Id { get; set; }
         public string Name { get; set; }
-		public string Description { get; set; }
+        public string Description { get; set; }
         public string User { get; set; }
     }
 }
